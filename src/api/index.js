230447c7ab636a214b588ac7a0e0e1bb98b79105
zakaryadev <zakaryamuratbaev13@gmail.com>
2023-03-04 15:16:03 +0500

    api key

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3/",
   params: {
-    key: "AIzaSyDgCyDH2kKphNp-hrSN4mg1s5trGy9JT7w",
+    key: "AIzaSyCFtbU22RX5RB8FW6HUQr--CxZvfg4J84Y",
   },
 });
 
